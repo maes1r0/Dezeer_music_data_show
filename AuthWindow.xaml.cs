9d17e0a3a65817cceca7f08246ab94bd3514fe56
Raynor Jim <gkmaes1r0@gmail.com>
2021-02-10 20:26:30 +0200

    Implement getting playlists, folowed artists, liked album, liked tracks

--- AuthWindow.xaml.cs
+++ AuthWindow.xaml.cs
@@ -21,47 +21,53 @@ namespace MusConv
     /// </summary>
     public partial class AuthWindow : Window
     {
-        private string appId = "app_id=461422&";
-        private string redirectUri = "redirect_uri=https://www.google.com&";
-        private string perms = "perms=basic_access,email,offline_access";
-        private string appSecret = "secret=777549cecb816c21db2fca0478eed61a&";
+        private string appId = "461422";
+        private string redirectUri = "https://www.google.com";
+        private string perms = "basic_access,email,offline_access";
+        private string appSecret = "777549cecb816c21db2fca0478eed61a";
+        HttpClient getAccessToken = new HttpClient();
 
-        public static string refreshToken { get; set; }
-        public static string accessToken { get; set; }
+        public static string RefreshToken { get; set; }
+        public static string AccessToken { get; set; }
         public AuthWindow()
         {
             InitializeComponent();
-            deezerAuth.Source = new Uri("https://connect.deezer.com/oauth/auth.php?" + appId + redirectUri + perms);
+            deezerAuth.Source = new Uri("https://connect.deezer.com/oauth/auth.php?app_id=" + appId + "&redirect_uri=" + redirectUri + "&perms=" + perms);
         }
 
-        
-        private async Task<string> GetAccessToken (string refreshToken)
+
+        private async Task<string> GetAccessToken(string refreshToken)
         {
-            using (HttpClient getAccessToken = new HttpClient())
+            using (getAccessToken)
             {
-               HttpResponseMessage response = getAccessToken.GetAsync("https://connect.deezer.com/oauth/access_token.php?" + appId + appSecret + refreshToken).Result;
+                HttpResponseMessage response = getAccessToken.GetAsync("https://connect.deezer.com/oauth/access_token.php?app_id="
+                    + appId + "&secret=" + appSecret + "&code=" + refreshToken).Result;
 
                 string accessToken = await response.Content.ReadAsStringAsync();
                 return accessToken;
             }
-
-
-                
         }
+    
 
         private void deezerAuth_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
         {
             string testingUri = e.Uri.ToString();
-            if (testingUri.Contains("https://www.google.com/?code="))
+            if (testingUri.Contains(redirectUri + "/?code="))
             {
-                this.Visibility = Visibility.Hidden;
-                refreshToken = testingUri.Substring(testingUri.IndexOf("code"));
-                accessToken = GetAccessToken(refreshToken).Result;
-                accessToken = accessToken.Substring(accessToken.IndexOf('=') + 1, accessToken.IndexOf('&') - accessToken.IndexOf('=') - 1);
-                MessageBox.Show(accessToken);
+                RefreshToken = testingUri.Substring(testingUri.IndexOf('=') + 1);
+                string result = GetAccessToken(RefreshToken).Result;
+                AccessToken = result.Substring(result.IndexOf('=') + 1, result.IndexOf('&') - result.IndexOf('=') - 1);
+                
+                //MessageBox.Show(AccessToken);
+                //deezerAuth.Dispose();
                 this.Close();
                 
             }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ((MainWindow)(this.Owner)).AccessToken = AccessToken;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Net.Http;
 
 
 namespace MusConv
@@ -22,6 +23,8 @@ namespace MusConv
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HttpClient generalClient = new HttpClient();
+        private string requestUri = "https://api.deezer.com/user/me";
         public string AccessToken { get; set; }
         public MainWindow()
         {
@@ -32,9 +35,57 @@ namespace MusConv
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             AuthWindow authWindow = new AuthWindow();
+            authWindow.Owner = this;
             authWindow.Show();
             
             
+        }
+
+        private void playlistRequest_Click(object sender, RoutedEventArgs e)
+        {
+            string result = GetPlaylistsAsync().Result;
+            MessageBox.Show(result);
+        }
+        private async Task<string> GetPlaylistsAsync()
+        {
+               HttpResponseMessage response = generalClient.GetAsync(requestUri + "/playlists?access_token=" + AccessToken).Result;
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+        }
+
+        private void folowedArtistsRequest_Click(object sender, RoutedEventArgs e)
+        {
+            string result = GetFolowedArtistsAsync().Result;
+            MessageBox.Show(result);
+        }
+        private async Task<string> GetFolowedArtistsAsync()
+        {
+            HttpResponseMessage response = generalClient.GetAsync(requestUri + "/artists?access_token=" + AccessToken).Result;
+            string result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        private void likedAlbumRequest_Click(object sender, RoutedEventArgs e)
+        {
+            string result = GetLikedAlbumAsync().Result;
+            MessageBox.Show(result);
+        }
+        private async Task<string> GetLikedAlbumAsync()
+        {
+            HttpResponseMessage response = generalClient.GetAsync(requestUri + "/albums?access_token=" + AccessToken).Result;
+            string result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+        private void LikedTrackRequest_Click(object sender, RoutedEventArgs e)
+        {
+            string result = GetLikedTrackAsync().Result;
+            MessageBox.Show(result);
+        }
+        private async Task<string> GetLikedTrackAsync()
+        {
+            HttpResponseMessage response = generalClient.GetAsync(requestUri + "/tracks?access_token=" + AccessToken).Result;
+            string result = await response.Content.ReadAsStringAsync();
+            return result;
         }
     }
 }
